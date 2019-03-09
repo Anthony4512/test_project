0fdf5c3727fea78d1e6b9a41ef8011c0f63a461b
Anthony Mirely <amirely@wayfair.com>
2019-03-09 14:30:04 -0500

    added forloop

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,12 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello sun!");
+        printForLoop();
+    }
+
+    public static void printForLoop() {
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Hello sun!");
+        }
     }
 }
